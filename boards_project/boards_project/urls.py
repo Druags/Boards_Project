9022f4629f49524a9f4bffb0357a745463d94e64
Druags <anty.troll@mail.ru>
2023-06-22 20:59:05 +0300

    base.html updated, it now displays authorized user. created accounts app with view for signup, created signup.html, updated urls file

--- boards_project/boards_project/urls.py
+++ boards_project/boards_project/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path, include
 from boards import views
+from accounts import views as accounts_views
+
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('signup/', accounts_views.signup, name='signup'),
     path('admin/', admin.site.urls),
     path('boards/<str:pk>', views.board_topics_view, name='board_topics'),
     path('boards/<str:pk>/new', views.new_topic, name='new_topic'),
