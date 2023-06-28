3e1fbba9f6027208c3653c81e4638fad90ec18cf
Druags <anty.troll@mail.ru>
2023-06-28 21:57:29 +0300

    added login, created template_tags for authorisation form, created template base_accounts

--- boards_project/boards_project/urls.py
+++ boards_project/boards_project/urls.py
@@ -25,6 +25,7 @@ from accounts import views as accounts_views
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('signup/', accounts_views.signup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     path('boards/<str:pk>', views.board_topics_view, name='board_topics'),
